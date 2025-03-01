3c2ed1f0efdede54cc14c11a9fb4d2018e975a5f
tonib <tonib@lsisa.com>
2025-03-01 10:09:29 +0100

    Call ollama

---
+++ AutocompleteVs/Generation.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.AI;
+using OllamaSharp.Models;
+using OllamaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutocompleteVs
+{
+	class Generation
+	{
+		/*static async public Task TestAsync()
+		{
+			IChatClient x = null;
+			if (x != null)
+			{
+				IAsyncEnumerable<ChatResponseUpdate> r = x.GetStreamingResponseAsync(null);
+			}
+
+			//IAsyncEnumerable<int> tuputamadre = null;
+			//IAsyncEnumerator<int> x = tuputamadre?.GetAsyncEnumerator();
+			//if(x != null)
+			//	_ = await x.MoveNextAsync();
+		}*/
+
+		async public static Task DoRequestAsync(string prefixText, string suffixText)
+		{
+
+			// set up the client
+			var uri = new Uri("http://localhost:11434");
+			var ollama = new OllamaApiClient(uri);
+
+			// select a model which should be used for further operations
+			ollama.SelectedModel = "Qwen2.5-Coder 1.5B";
+
+			var request = new GenerateRequest();
+			request.Prompt = prefixText;
+			request.Suffix = suffixText;
+			/*
+			IAsyncEnumerator<GenerateResponseStream> enumerator = ollama.GenerateAsync(request).GetAsyncEnumerator();
+			while(await enumerator.MoveNextAsync())
+			{
+				Debug.WriteLine(enumerator.Current.Response);
+			}*/
+		}
+	}
+}
---
+++ AutocompleteVs/TextEditorUtils.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutocompleteVs
+{
+	class TextEditorUtils
+	{
+		// For code to get current editor, see:
+		// https://stackoverflow.com/questions/37469869/get-roslyn-syntaxtoken-from-visual-studio-text-selection-caret-position
+		// https://vlasovstudio.com/visual-commander/commands.html#CreateTypedVariable
+
+		static public Microsoft.VisualStudio.Text.Editor.IWpfTextView GetTextView(AsyncPackage package)
+		{
+			var textManager = package.GetService<
+					Microsoft.VisualStudio.TextManager.Interop.SVsTextManager,
+					Microsoft.VisualStudio.TextManager.Interop.IVsTextManager>();
+
+			Microsoft.VisualStudio.TextManager.Interop.IVsTextView textView;
+			textManager.GetActiveView(1, null, out textView);
+			return GetEditorAdaptersFactoryService(package).GetWpfTextView(textView);
+}
+
+		static private Microsoft.VisualStudio.Editor.IVsEditorAdaptersFactoryService GetEditorAdaptersFactoryService(AsyncPackage package)
+		{
+			var componentModel = package.GetService<
+					Microsoft.VisualStudio.ComponentModelHost.SComponentModel,
+					Microsoft.VisualStudio.ComponentModelHost.IComponentModel>();
+			return componentModel.GetService<Microsoft.VisualStudio.Editor.IVsEditorAdaptersFactoryService>();
+		}
+	}
+}
