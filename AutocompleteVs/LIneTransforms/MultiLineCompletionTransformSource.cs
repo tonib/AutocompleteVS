e67a83b002fdd2810b3a6cfc3665868b34000d1a
tonib <tonib@lsisa.com>
2025-03-07 20:26:24 +0100

    Line transformations test

---
+++ AutocompleteVs/LIneTransforms/MultiLineCompletionTransformSource.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+using Microsoft.VisualStudio.TextManager.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutocompleteVs.LIneTransforms
+{
+	class MultiLineCompletionTransformSource : ILineTransformSource
+	{
+		private IWpfTextView View;
+
+		private MultiLineCompletionTransformSource(IWpfTextView view)
+		{
+			View = view;
+		}
+
+		static public MultiLineCompletionTransformSource Attached(IWpfTextView view)
+			=> view.Properties.GetOrCreateSingletonProperty(() => new MultiLineCompletionTransformSource(view));
+
+		public LineTransform GetLineTransform(ITextViewLine line, double yPosition, ViewRelativePosition placement)
+		{
+			return new LineTransform(0, 50, 1.0);
+		}
+	}
+}
---
+++ AutocompleteVs/LIneTransforms/MultiLineCompletionTransformSourceProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutocompleteVs.LIneTransforms
+{
+	[Export(typeof(ILineTransformSourceProvider))]
+	[ContentType("text")]
+	[TextViewRole(PredefinedTextViewRoles.Interactive)]
+	internal sealed class MultiLineCompletionTransformSourceProvider : ILineTransformSourceProvider
+	{
+		public ILineTransformSource Create(IWpfTextView textView)
+		{
+			return MultiLineCompletionTransformSource.Attached(textView);
+		}
+	}
+}
