7f1abeb0634f33e0c99a36a7f2d1b8abbff98166
tonib <tonib@lsisa.com>
2025-03-19 19:37:24 +0100

    Added class for openai generation (test)

---
+++ AutocompleteVs/SuggestionGeneration/OpenAiGenerator.cs
@@ -0,0 +1,32 @@
+﻿using OllamaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutocompleteVs.SuggestionGeneration
+{
+	/// <summary>
+	/// Suggestions generation with OpenAI API
+	/// </summary>
+	class OpenAiGenerator : IGenerator
+	{
+
+		public OpenAiGenerator(Settings settings)
+		{
+			
+		}
+
+		public void Dispose()
+		{
+
+		}
+
+		public Task GetAutocompletionInternalAsync(GenerationParameters parameters, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
